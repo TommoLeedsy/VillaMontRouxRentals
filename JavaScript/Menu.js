2172405de05048b769850d59a00c14a6cbe8d662
Thomas Leeds <tommo.leedsy@gmail.com>
2016-07-12 12:15:58 +0100

    Optimization

--- JavaScript/Menu.js
+++ JavaScript/Menu.js
@@ -1,42 +1,42 @@
-var main = function() {
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+var main = function () {
+        $('.menu-open').click(function () {
+            $('.menu').animate({
+                left: "0px"
+            }, 200);
 
-    $('body').animate({
-      left: "300px"
-    }, 200);
-  });
+            $('body').animate({
+                left: "300px"
+            }, 200);
+        });
 
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-300px"
-    }, 200);
+        $('.menu-close').click(function () {
+            $('.menu').animate({
+                left: "-300px"
+            }, 200);
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+            $('body').animate({
+                left: "0px"
+            }, 200);
+        });
 
-  $('.main').on("swiperight", function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+        $('.main').on("swiperight", function () {
+            $('.menu').animate({
+                left: "0px"
+            }, 200);
 
-    $('body').animate({
-      left: "300px"
-    }, 200);
-  });
+            $('body').animate({
+                left: "300px"
+            }, 200);
+        });
 
-  $('.menu').on("swipeleft", function() {
-    $('.menu').animate({
-      left: "-300px"
-    }, 200);
+        $('.menu').on("swipeleft", function () {
+            $('.menu').animate({
+                left: "-300px"
+            }, 200);
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-};
+            $('body').animate({
+                left: "0px"
+            }, 200);
+        });
+    };
 $(document).ready(main);

cbd10c6d163416f18da6aa7f80950b32a41d47cd
TommoLeedsy <tommo.leedsy@gmail.com>
2016-08-10 21:45:40 +0200

    Finish Template content needed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     // MOUSE WHEEL
     $('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        $('.owl-carousel').trigger('next.owl');
-    } else {
-        $('.owl-carousel').trigger('prev.owl');
-    }
-    e.preventDefault(); 
+        if (e.deltaY > 0) {
+            $('.owl-carousel').trigger('next.owl');
+        } else {
+            $('.owl-carousel').trigger('prev.owl');
+        }
+        e.preventDefault();
     });
     //hide the all of the element with class msg_body
     $(".expanded-text").hide();
     //toggle the componenet with class msg_body
-    $(".toggle-expand").click(function()
-    {
-    $(this).next(".expanded-text").slideToggle(600);
+    $(".toggle-expand").click(function () {
+        $(this).next(".expanded-text").slideToggle(600);
+    });
+    $("#clearfix").css({"height": $("header").height() });
+    $("#mask,.mob-nav").css({"margin-top": $("header").height() });
+    $('.mob-show').click(function () {
+        $('#mask').fadeIn(500);
+        $('.mob-nav').css('right', '0');
+    });
+    $('#mask').click(function () {
+        $('.mob-nav').css('right', '-270px');
+        $('#mask').fadeOut(500);
     });
 });
